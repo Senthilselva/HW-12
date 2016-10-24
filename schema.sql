21ba5eed76bcebd38a383b9e1c258c2e1b1e1195
Senthilselva <senthilselvak@gmail.com>
2016-10-23 22:32:25 -0400

    initizal commit

---
+++ schema.sql
@@ -0,0 +1,23 @@
+-- Creates the "Bamazon_db" database --
+CREATE DATABASE Bamazon_db;
+
+-- Makes it so all of the following code will affect Bamazon_db --
+USE Bamazon_db;
+
+-- Creates the table "Products" within additional_favorites_db --
+CREATE TABLE Products (
+	-- Creates a numeric column called "itemId" which will automatically increment its default value as we create new rows --
+	itemId INTEGER(11) AUTO_INCREMENT NOT NULL,
+	-- Makes a string column called "productName" which cannot contain null --
+    productName VARCHAR(30) NOT NULL,
+    -- Makes a string column called "departmentName" which cannot contain null --
+    departmentName VARCHAR(30) NOT NULL,
+	-- Makes an numeric column called "price" --
+    price INTEGER(10,2),
+	-- Makes an numeric column called "stockQuantity" --
+    stockQuantity INTEGER(10),
+    -- Sets itemId as this table's primary key which means all data contained within it will be unique --
+    PRIMARY KEY (itemId)
+
+);
+
